426d80ffbf81f073b7ac8f1988b1c2158ead68ab
Bronson <bronsonmathews@gmail.com>
2018-04-04 20:36:22 +0930

    - updated app name

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,21 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QCoreApplication::setApplicationName(QStringLiteral("Audio Book"));
+
+/*
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setApplicationName(APP_NAME);
+    QCoreApplication::setApplicationVersion(APP_VERSION);
+    QCoreApplication::setOrganizationDomain(APP_URL);
+    QCoreApplication::setWindowIcon(QIcon(APP_ICON_PATH));
+    */
+
+    QCoreApplication::setApplicationName(QStringLiteral("AudioBook"));
     QCoreApplication::setApplicationVersion( QLatin1String(QT_VERSION_STR));
     QCoreApplication::setOrganizationName(QStringLiteral("bit-shift"));
     QCoreApplication::setOrganizationDomain("bit-shift.io");
-    QGuiApplication::setApplicationDisplayName(QStringLiteral("Audio Book"));
+    QGuiApplication::setApplicationDisplayName(QStringLiteral("AudioBook"));
 
     MainWindow *main_window = new MainWindow();
     main_window->show();
